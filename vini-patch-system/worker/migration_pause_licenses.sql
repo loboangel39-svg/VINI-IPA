b51e680071dc39d9b31c2d87e67b7ea8a86af12e
loboangel39-svg <loboangel39@gmail.com>
2026-09-04 13:17:26 -0500

    Delete vini-patch-system directory

--- vini-patch-system/worker/migration_pause_licenses.sql
+++
@@ -1,8 +0,0 @@
--- Migración: Agregar columna 'paused' a la tabla licenses
--- Ejecutar en D1: wrangler d1 execute vini-patch-db --file=migration_pause_licenses.sql
-
--- Columna para pausar licencias individualmente
-ALTER TABLE licenses ADD COLUMN paused INTEGER DEFAULT 0;
-
--- Índice para consultas rápidas de licencias pausadas
-CREATE INDEX IF NOT EXISTS idx_licenses_paused ON licenses(paused);
